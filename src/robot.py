d3bea2790a888d125dd63971fb3639474abe68b3
DavidCroce <126100052+DavidCroce@users.noreply.github.com>
2023-11-27 11:11:22 +0100

    robot module and testing in main

--- src/robot.py
+++ src/robot.py
@@ -1,28 +1,30 @@
 # Create a class for the robot
 # Class contains parameters such as speed, state...
 # It also contains the states like position, angle...
-__init__ = ['robot']
+import numpy as np
 
+__init__ = ['robot']
 
 class robot:
     def __init__(self):
-        self.pos = [0,0]                 # position of the robot
-        self.pos_marker_top = [0,0]      # position of the top marker
-        self.pos_marker_bottom = [0,0]   # position of the bottom marker
-        self.phi = 0                     # angle relative to map 
-        self.teta = 0                    # angle relative to trajectory
-        self.speed = [0,0]               #speed of right and left wheels
-        self.state = 'STOP'              # state of the robot
+        
+        self.pos_marker_top = np.array([0,0])                           # position of the top marker
+        self.pos_marker_bottom = np.array([0,0])                        # position of the bottom marker
+        self.pos = np.array([0,0])                                      # position of the robot
+        self.pos = (self.pos_marker_top + self.pos_marker_bottom) / 2   # position of the robot
+        self.phi = np.arctan2(self.pos_marker_top[1] - self.pos_marker_bottom[1],
+                               self.pos_marker_top[0] - self.pos_marker_bottom[0])
+        self.phi_dot = 0                                                # angular velocity of the robot
+        self.teta = 0                                                   # trajectory angle of the robot
+        self.speed = np.array([0,0])                                    #speed of right and left wheels
+        self.state = 'STOP'                                             # state of the robot
 
     def go_forward(self,speed):
         self.state = 'FORWARD'  # state of the robot
         self.speed = [speed,speed]  # speed of right and left wheels
         v = {"motor.left.target": [50],
              "motor.right.target": [50],}
-        return v
-        #node.send_set_variables(v)
-
-        
+        return v        
 
     def turn(self,speed):
         self.state = 'TURN'     # state of the robot
@@ -33,22 +35,28 @@ class robot:
             self.speed = [speed,-speed] # turn right
             v = {"motor.left.target": [self.speed[0]],
                  "motor.right.target": [self.speed[1]],}
-        #node.send_set_variables(v)
+        return v
 
     def stop(self):
         self.state = 'STOP'     # state of the robot
         self.speed = [0,0]      # speed of right and left wheels 
         v = {"motor.left.target": [0],
              "motor.right.target": [0],}
-        return 'STOPPED'
-        #node.send_set_variables(v)
+        return v
+    
+    def update(self, trajectory_angle):
+        self.pos = (np.add(self.pos_marker_top,self.pos_marker_bottom))/2  
+        self.phi = np.arctan2(self.pos_marker_top[1] - self.pos_marker_bottom[1],
+                               self.pos_marker_top[0] - self.pos_marker_bottom[0])
+        self.teta = self.phi - np.deg2rad(trajectory_angle)                         
         
     def print_status(self):
         print('Robot position: ',self.pos)
-        print('Robot angle: ',self.phi)
+        print('Robot angle: ',np.rad2deg(self.phi))
+        print('Robot angular velocity: ',self.phi_dot)
         print('Robot speed: ',self.speed)
         print('Robot state: ',self.state)
-        print('Robot trajectory angle: ',self.teta)
+        print('Robot trajectory angle: ',np.rad2deg(self.teta))
         print('Robot top marker position: ',self.pos_marker_top)
         print('Robot bottom marker position: ',self.pos_marker_bottom)
         print('---------------------------')
