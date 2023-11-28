fa99a324f66203972694f36cddcc58f96714ad4d
Le0wl <leonie.gasser@epfl.ch>
2023-11-28 18:49:59 +0100

    with obstacle threshhold

--- src/path.py
+++ src/path.py
@@ -5,8 +5,9 @@ from matplotlib import colors
 import astar as a
 
 class Map:
-    def __init__(self, obstacles):
-        self.grid = self.init_grid(obstacles)   
+    def __init__(self, obstacles, scale):
+        self.grid = self.init_grid(obstacles)
+        self.collision_proof = self.margin(obstacles, scale)
 
     def init_grid(self, obstacles):
         size = (50, 50)
@@ -20,12 +21,25 @@ class Map:
                 for j in range(width):
                     grid[beginning[0] + i][beginning[1]+ j] = 1
         return(grid)
-
+    
+    def margin(self, obstacles, scale):
+        size = (50, 50)
+        grid = np.zeros(size, dtype=int)
+        for k in range(len(obstacles)):
+            obsta = obstacles[k]
+            beginning = obsta[0]
+            end = obsta[1]
+            hight,width = end[0]-beginning[0]+2*scale,end[1]-beginning[1]+2*scale
+            for i in range(hight):
+                for j in range(width):
+                    grid[beginning[0] + i - scale][beginning[1]+ j- scale] = 1
+        return(grid)
+    
     def __len__(self):
         return(len(self.grid))
     
     def get_map(self):
-        return(self.grid)
+        return(self.collision_proof)
     
     def getElement(self, i, j):
         return self.grid[i][j]
@@ -40,15 +54,30 @@ class Map:
         plt.imshow(self.grid, cmap=cmap, interpolation='nearest')
         plt.show()
 
+def robo_pos(dots):
+    robot = (1,1)
+    nose, tail= dots[0],dots[1]
+    robot = round((nose[0]+tail[0])/2),round((nose[1]+tail[1])/2)
+    scale = round(np.sqrt((nose[0]-tail[0])^2+(nose[1]-tail[1])^2))
+    return(robot, scale)
 
-
-def path(robot, goal, obstacles):
-    #robot = (0, 0)
+def get_path(dots, goal, obstacles):
+    #dots = [(0, 0),(2, 3)]
     #goal = (17, 46)
     #obstacles = [((5,5),(10,7)),((36,5),(40,20))]
-    maze = Map(obstacles)
-
+    robot, scale = robo_pos(dots)
+    maze = Map(obstacles, scale)
     path = a.astar(maze.get_map(), robot, goal)
     maze.plot_map(path)
     return(path)
 
+def main():
+    dots = [(0, 0),(2, 3)]
+    goal = (30, 20)
+    obstacles = [((5,5),(10,7)),((36,5),(40,20)),((16,5),(20,20))] 
+    weg = get_path(dots, goal, obstacles)
+    print(weg)
+    return()
+
+if __name__ == "__main__":
+    main()
