63499bdc22ee2c9329d9e5d2fd29a7258250304a
Le0wl <leonie.gasser@epfl.ch>
2023-12-04 14:08:03 +0100

    updated

--- src/path.py
+++ src/path.py
@@ -88,7 +88,7 @@ def get_path(robot, goal, obstacles):
 def main():
     robot = np.array([2, 3])
     goal = np.array([40, 30])
-    obstacles = np.array([[[5,5],[10,20]],[[36,5],[40,20]],[[16,5],[20,20]]]) 
+    obstacles = np.array([[[5,15],[10,30]],[[36,5],[40,15]],[[16,5],[20,20]]]) 
     weg = get_path(robot, goal, obstacles)
     print(weg)
     return()
