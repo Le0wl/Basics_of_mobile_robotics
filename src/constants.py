5ed6bda9cd7c2902149dc954cb4beea9aba0245a
DavidCroce <126100052+DavidCroce@users.noreply.github.com>
2023-12-07 11:30:41 +0100

    working local avoidance

--- src/constants.py
+++ src/constants.py
@@ -1,9 +1,13 @@
-UNIT_NUMBER = 15  # number of units in the map
-PIXEL_MARGIN = 15 # margin in pixels (to set the position of the objects)
+UNIT_NUMBER = 12  # number of units in the map
+PIXEL_MARGIN = 18 # margin in pixels (to set the position of the objects)
 
 OBSTACLE_MARGIN = 3     # margin in units (becaust the robot has dimensions)
 RATIO = 1.2             # To manually adjust for the camera angle
-DISTANCE_THRESHOLD = 50 # To manually adjust for the camera angle
+DISTANCE_THRESHOLD = 30 # To manually adjust for the camera angle
 
 FREE = 0
 OBSTACLE = 1
+
+USE_KALMAN = True
+
+ROBOT_DIAMETER = 80 # pixels
--- src/main.py
+++ src/main.py
@@ -7,81 +7,100 @@ import sys
 sys.path.append('.\src')
 
 from robot import *
-import aruco
-from aruco import *
+import vision
+from vision import *
 from env import *
 from constants import *
 from astar_path_planning import *
 from kalman import *
 
-hist_pos = np.zeros((2, 1))
-hist_camera = np.zeros((2,1))
+class history:
+    def __init__(self):
+        self.hist_pos = np.array([0,0])
+        self.hist_camera = np.array([0,0])
 
-map_base = environment()
-robot = robot()
-kalman = Kalman()
+history = history()
+
+
+#============================= Create instances ======================================================
+map_base = environment() # Create an instance for the environment
+robot = robot()          # Create an instance for the robot
+kalman = Kalman()        # Create an instance for the Kalman filter
 
 markers = [ArucoMarker(marker_id) for marker_id in range(1, 6)]  # Create instances for each ArUco marker
 
 print("Starting camera...")
 
 # Create a thread for the ArUco marker detection
-aruco_detect = threading.Thread(target=main_aruco, args=(*markers,))
+aruco_detect = threading.Thread(target=main_vision, args=(*markers,))
+
 # Create a thread for the robot
 robot_th = threading.Thread(target=robot.run_robot, args=())
 
+# Start the threads
 aruco_detect.start()
 robot_th.start()
 
 time.sleep(3)
 print("Running threads...")
 
+#============================= MAIN LOOP =============================================================
+
 def update_main(speed, angular_speed):
 
     ex_time = time.time()
     camera_blocked = markers[0].camera_blocked
 
-#============================= SETTING MAP CORNERS ======================================================
+#============================= SETTING MAP CORNERS ===================================================
     map_base.top_left = markers[1].pos
     map_base.top_right = markers[2].pos
     map_base.bottom_left = markers[3].pos
     map_base.bottom_right = markers[4].pos
 
-#============================= MAP UPDATE ======================================================
-    x_est, P_est = kalman.kalman_filter(markers[0].pos[0], markers[0].pos[1], markers[0].angle, speed, angular_speed, camera_blocked)
+#============================= KALMAN FILTER =========================================================
 
-    markers[0].robot_est = x_est
+    if USE_KALMAN:
+        x_est, P_est = kalman.kalman_filter(markers[0].pos[0], markers[0].pos[1], markers[0].angle, speed, angular_speed, camera_blocked)
 
-    robot.pos = x_est[0:2]
+        markers[0].robot_est = x_est # Estimated position of the robot
 
-    robot.phi = x_est[2]
-  
-    distance_vertical = map_base.get_vertical_distance()
+        robot.pos = x_est[0:2]       # Position of the robot (estimated by the Kalman filter)
+
+        robot.phi = x_est[2]         # Angle of the robot (estimated by the Kalman filter)
+    else:
+        robot.pos = markers[0].pos
+        robot.phi = markers[0].angle
+    
+
+#============================= MAP GENERATION ========================================================
+
+    distance_vertical = map_base.get_vertical_distance()  
     distance_horizontal = map_base.get_horizontal_distance()
 
-    map_base.map = np.zeros((UNIT_NUMBER,UNIT_NUMBER,2))
-
- #============================= MAP GENERATION ======================================================
+    map_base.map = np.zeros((UNIT_NUMBER,UNIT_NUMBER,2)) # Create a map of the environment (N x N x 2 matrix)
 
     for i in range(UNIT_NUMBER):
         for j in range(UNIT_NUMBER):
             
+            # Calculate the position of each unit in the map with a manual calibration (RATIO)
             x_pos = j*RATIO + (2*i+1)*(distance_horizontal - j*2*RATIO)/(2*UNIT_NUMBER)
             y_pos = -(2*j+1)*distance_vertical/(2*UNIT_NUMBER)
 
             map_base.map[i,j] = map_base.top_left + np.array([x_pos,y_pos])
            
+#============================= MAP UPDATE ============================================================
     vision.set_map(map_base.map)
 
-    goal_idx = markers[4].goal_idx
+    goal_idx = markers[4].goal_idx # Index of the goal in the map
 
-    rob_idx = markers[0].robot_idx
+    rob_idx = markers[0].robot_idx # Index of the robot in the map
 
-    map_base.map = markers[4].Map_indices
+    map_base.map = markers[4].map_obstacles # Map with obstacles
+    #print(map_base.map)
 
-    path = get_path_rect(map_base.map,rob_idx, goal_idx)
+    path = get_path(map_base.map,rob_idx, goal_idx)
     
-    markers[0].path = path
+    markers[0].path = path # Path of the robot
 
     goal_coord = vision.Map_camera[goal_idx[0]][goal_idx[1]]
 
@@ -95,28 +114,35 @@ def update_main(speed, angular_speed):
   
 #============================= ANGLE CALCULATION ====================================================
 
+    # Calculate the angle of the robot-next ponit line
     angle =np.rad2deg(np.arctan2(-robot.trajectory[1] + robot.pos[1],robot.trajectory[0] - robot.pos[0])) + 180
 
+    # Angle correction
     if angle > 180:
         angle = angle - 360
-        
+    
+    # Calculate the angle between the robot and the next point
     robot.teta = robot.phi - angle + 180
 
+    # Angle correction
     if(robot.teta > 180):
         robot.teta = robot.teta - 360
         
     time.sleep(0.1)
 #=========================== OUTPUT IS SPEED OF MOTORS =============================================
-    v = robot.v
+    
 
     #print("Execution time: ", time.time() - ex_time)
-    #np.append(hist_pos, robot.pos, axis=1)
-    #np.append(hist_camera, markers[0].pos, axis=1)
+    history.hist_pos = np.append(history.hist_pos, robot.pos[0])
+    history.hist_camera = np.append(history.hist_camera, markers[0].pos[0])
+    #print("hist_pos: ", hist_pos)
 
-    return v
+    v = robot.v
+
+    return v, history.hist_pos, history.hist_camera
     
 
 if __name__ == "__main__":
     while True:
-        update_main()
+        update_main(speed=0, angular_speed=0)
        
